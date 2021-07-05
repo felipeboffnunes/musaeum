ec98d7ad3934eadc8ac67719b0290b866c279d6d
Felipe.Nunes <felipe.nunes@bairesdev.com>
2021-07-04 22:44:46 -0300

    Removed layout register.

--- system/register/layout.py
+++ system/register/layout.py
@@ -1,11 +1,1 @@
 
-# Third party imports
-import dash_core_components as dcc
-import dash_html_components as html
-
-# Local application/library specific imports
-from system.layouts.fragments.menu import sidebar
-
-def register_layout(app):
-        content = html.Div(id="page-content")
-        app.layout = html.Div([dcc.Location(id="url"), html.Div(id="inner-layout")], id="layout")
