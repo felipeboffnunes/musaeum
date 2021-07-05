8c43b38d9c3fda0704664a890fec76aa8e099cbb
Felipe.Nunes <felipe.nunes@bairesdev.com>
2021-07-04 22:45:27 -0300

    Wrapped sidebar in a method to pass it collapsed when suited.

--- system/layouts/fragments/menu.py
+++ system/layouts/fragments/menu.py
@@ -1,89 +1,85 @@
-
-# Third party imports
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 import dash_html_components as html
 
 
-# we use the Row and Col components to construct the sidebar header
-# it consists of a title, and a toggle, the latter is hidden on large screens
-sidebar_header = dbc.Row(
-    [
-        dbc.Col(html.P("Musaeum"), id="musaeum-header"),
-        dbc.Col(
-            [
-                html.Button(
-                    # use the Bootstrap navbar-toggler classes to style
-                    html.Span(className="navbar-toggler-icon"),
-                    className="navbar-toggler",
-                    # the navbar-toggler classes don't set color
-                    style={
-                        "color": "rgba(0,0,0,.5)",
-                        "border-color": "rgba(0,0,0,.1)",
-                    },
-                    id="navbar-toggle",
-                ),
-                html.Button(
-                    # use the Bootstrap navbar-toggler classes to style
-                    html.Span(className="navbar-toggler-icon"),
-                    className="navbar-toggler",
-                    # the navbar-toggler classes don't set color
-                    style={
-                        "color": "rgba(0,0,0,.5)",
-                        "border-color": "rgba(0,0,0,.1)",
-                    },
-                    id="sidebar-toggle",
-                ),
-            ],
-            # the column containing the toggle will be only as wide as the
-            # toggle, resulting in the toggle being right aligned
-            width="auto",
-            # vertically align the toggle in the center
-            align="center",
-        ),
-    ]
-)
+def get_sidebar(collapse: bool = False):
+    if collapse:
+        collapsed = "collapsed"
+    else:
+        collapsed = "simple",
 
-sidebar = html.Div(
-    [
-        sidebar_header,
-        html.Hr(id="line-logo"),
-        html.P("not a university"),
-        # we wrap the horizontal rule and short blurb in a div that can be
-        # hidden on a small screen
-        html.Div(
-            [
-                html.Hr(),
-            ],
-            id="blurb",
-        ),
-        # use the Collapse component to animate hiding / revealing links
-        dbc.Collapse([
-            dbc.Nav(
+    sidebar_header = dbc.Row(
+        [
+            dbc.Col(html.P("Musaeum"), id="musaeum-header"),
+            dbc.Col(
                 [
-                    dbc.NavLink("Home", href="/home", active="exact"),
-                    dbc.NavLink("About", href="/about", active="exact"),
-                    dbc.NavLink("Wiki", href="https://wiki.musaeum.university")
+                    html.Button(
+                        html.Span(className="navbar-toggler-icon"),
+                        className="navbar-toggler",
+                        style={
+                            "color": "rgba(0,0,0,.5)",
+                            "border-color": "rgba(0,0,0,.1)",
+                        },
+                        id="navbar-toggle",
+                    ),
+                    html.Button(
+                        html.Span(className="navbar-toggler-icon"),
+                        className="navbar-toggler",
+                        style={
+                            "color": "rgba(0,0,0,.5)",
+                            "border-color": "rgba(0,0,0,.1)",
+                        },
+                        id="sidebar-toggle",
+                    ),
                 ],
-                vertical=True,
-                pills=True,
+                width="auto",
+                align="center",
             ),
-            html.Div([
-                html.Hr(id="line-logo"),
-                dbc.Col(html.P("Pyrrhic Buddha", id="pyrrhic-header")),
+        ]
+    )
+
+    sidebar = html.Div(
+        [
+            sidebar_header,
+            html.Hr(id="line-logo"),
+            html.P("not a university"),
+
+            html.Div(
+                [
+                    html.Hr(),
+                ],
+                id="blurb",
+            ),
+
+            dbc.Collapse([
                 dbc.Nav(
                     [
-                        dbc.NavLink("Library", href="/library", active="exact"),
-                        dbc.NavLink("Logbook", href="/logbook", active="exact"),
-                        dbc.NavLink("Writing", href="/writing", active="exact"),
+                        dbc.NavLink("Home", href="/home", active="exact"),
+                        dbc.NavLink("About", href="/about", active="exact"),
+                        dbc.NavLink("Wiki", href="https://wiki.musaeum.university")
                     ],
                     vertical=True,
                     pills=True,
-                )
-            ], id="pyrrhic-sidebar")
+                ),
+                html.Div([
+                    html.Hr(id="line-logo"),
+                    dbc.Col(html.P("Pyrrhic Buddha", id="pyrrhic-header")),
+                    dbc.Nav(
+                        [
+                            dbc.NavLink("Library", href="/library", active="exact"),
+                            dbc.NavLink("Logbook", href="/logbook", active="exact"),
+                            dbc.NavLink("Writing", href="/writing", active="exact"),
+                        ],
+                        vertical=True,
+                        pills=True,
+                    )
+                ], id="pyrrhic-sidebar")
             ],
-            id="collapse",
-        ),
-    ],
-    id="sidebar",
-)
+                id="collapse",
+            ),
+        ],
+        className=collapsed if isinstance(collapsed, str) else collapsed[0],
+        id="sidebar",
+    )
+
+    return sidebar
--- system/register/callbacks.py
+++ system/register/callbacks.py
@@ -1,143 +1,141 @@
-# Third party imports
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input, State
-# Local application/library specific imports
+
 from system.layouts.pages.pages_index import pages
-from system.layouts.fragments.menu import sidebar
+from system.layouts.fragments.menu import get_sidebar
 from system.layouts.pages.info.logbook_handler import LogBook_Handler
 from system.layouts.pages.info.library_info import authors
 
-def register_callbacks(app):
 
+def register_callbacks(app):
     # Navigator
-    #
-    @app.callback(Output("inner-layout", "children"), [Input("url", "pathname")])
-    def render_page_content(pathname): 
+    # -----------+
+    @app.callback(
+        Output("inner-layout", "children"),
+        [Input("url", "pathname")]
+    )
+    def render_page_content(pathname):
         if pathname == "/":
             content = html.Div([pages["HER"]], id="page-content")
             return None, content
         elif pathname == "/home":
             content = html.Div([pages["HOM"]], id="page-content")
-            return sidebar, content
+            return get_sidebar(), content
         elif pathname == "/library":
             content = html.Div([pages["LIB"]], id="page-content")
-            return sidebar, content
+            return get_sidebar(), content
         elif pathname == "/logbook":
             content = html.Div([pages["LOG"]], id="page-content")
-            return sidebar, content
+            return get_sidebar(), content
         elif pathname == "/writing":
             content = html.Div([pages["WRT"]], id="page-content")
-            return sidebar, content
-        # If the user tries to reach a different page, return a 404 message
-        return sidebar, html.Div([dbc.Jumbotron(
+            return get_sidebar(), content
+        elif pathname == "/about":
+            content = html.Div([pages["ABT"]], id="page-content")
+            return get_sidebar(collapse=True), content
+
+        return get_sidebar(), html.Div([dbc.Jumbotron(
             [
                 html.H1("404: Not found", className="text-danger"),
                 html.Hr(),
                 html.P(f"The pathname {pathname} was not recognised..."),
             ]
         )], id="page-content")
-        
+
     # Collapse Sidebar
-    #
+    # ---------------+
     @app.callback(
-    Output("sidebar", "className"),
-    [Input("sidebar-toggle", "n_clicks")],
-    [State("sidebar", "className")],
+        Output("sidebar", "className"),
+        Input("sidebar-toggle", "n_clicks"),
+        State("sidebar", "className"),
     )
-    def toggle_classname(n, classname):
-        if n and classname == "":
+    def toggle_collapse(n, classname):
+        if n and classname == "simple":
             return "collapsed"
-        return ""
-    #
-    # 
-    @app.callback(
-    Output("collapse", "is_open"),
-    [Input("navbar-toggle", "n_clicks")],
-    [State("collapse", "is_open")],
-    )
-    def toggle_collapse(n, is_open):
         if n:
-            return not is_open
-        return is_open
-    
+            return "simple"
+        return classname
+    # -----------------+
+
     # Logbook
-    #
+    # -----------+
     @app.callback(
-    Output("logbook-div", "children"),
-    [Input("day-picker", "date")]
+        Output("logbook-div", "children"),
+        [Input("day-picker", "date")]
     )
     def update_health(date_str):
         lg_handler = LogBook_Handler()
         return lg_handler.consume_data(date_str)
-    #
+    # -----------------------------------------+
     # Logbook Modal
-    #
+    # ------------+
     @app.callback(
-    [Output("logbook-act-modal", "is_open"),
-     Output("logbook-act-modal-body", "children"),
-     Output("logbook-act-modal-header", "children")],
-    Input("ACTINFO", "n_clicks"),
-    State("day-picker", "date")
+        [Output("logbook-act-modal", "is_open"),
+         Output("logbook-act-modal-body", "children"),
+         Output("logbook-act-modal-header", "children")],
+        Input("ACTINFO", "n_clicks"),
+        State("day-picker", "date")
     )
     def open_activities_modal(act_clicks, date_str):
         if act_clicks:
             lg_handler = LogBook_Handler()
             return [True, lg_handler.graphs(date_str, "ACT"), "ACTION"]
         return [dash.no_update, dash.no_update, dash.no_update]
-    #
+    # --------------------------------------------------------+
     @app.callback(
-    [Output("logbook-stu-modal", "is_open"),
-     Output("logbook-stu-modal-body", "children"),
-     Output("logbook-stu-modal-header", "children")],
-    Input("STUINFO", "n_clicks"),
-    State("day-picker", "date")
+        [Output("logbook-stu-modal", "is_open"),
+         Output("logbook-stu-modal-body", "children"),
+         Output("logbook-stu-modal-header", "children")],
+        Input("STUINFO", "n_clicks"),
+        State("day-picker", "date")
     )
     def open_studies_modal(stu_clicks, date_str):
         if stu_clicks:
             return [True, "STUINFO", "STUINFO-yes"]
         return [dash.no_update, dash.no_update, dash.no_update]
-    #
+    # --------------------------------------------------------+
     @app.callback(
-    [Output("logbook-nut-modal", "is_open"),
-     Output("logbook-nut-modal-body", "children"),
-     Output("logbook-nut-modal-header", "children")],
-    Input("NUTINFO", "n_clicks"),
-    State("day-picker", "date")
+        [Output("logbook-nut-modal", "is_open"),
+         Output("logbook-nut-modal-body", "children"),
+         Output("logbook-nut-modal-header", "children")],
+        Input("NUTINFO", "n_clicks"),
+        State("day-picker", "date")
     )
     def open_nutrition_modal(nut_clicks, date_str):
         if nut_clicks:
             lg_handler = LogBook_Handler()
             return [True, lg_handler.graphs(date_str, "NUT"), "Nutrition"]
         return [dash.no_update, dash.no_update, dash.no_update]
-    #
+    # --------------------------------------------------------+
     @app.callback(
-    [Output("logbook-exe-modal", "is_open"),
-     Output("logbook-exe-modal-body", "children"),
-     Output("logbook-exe-modal-header", "children")],
-    Input("EXEINFO", "n_clicks"),
-    State("day-picker", "date")
+        [Output("logbook-exe-modal", "is_open"),
+         Output("logbook-exe-modal-body", "children"),
+         Output("logbook-exe-modal-header", "children")],
+        Input("EXEINFO", "n_clicks"),
+        State("day-picker", "date")
     )
     def open_exercises_modal(exe_clicks, date_str):
         if exe_clicks:
             lg_handler = LogBook_Handler()
             return [True, lg_handler.graphs(date_str, "EXE"), "Exercises"]
         return [dash.no_update, dash.no_update, dash.no_update]
-    
+    # --------------------------------------------------------+
+
     # Library
-    #
+    # -----------+
     @app.callback(
-    [Output("library-modal", "is_open"),
-     Output("library-modal-body", "children"),
-     Output("library-modal-header", "children")],
-    [Input("library-datatable", "active_cell"),
-     Input("library-datatable", "derived_viewport_data")]
+        [Output("library-modal", "is_open"),
+         Output("library-modal-body", "children"),
+         Output("library-modal-header", "children")],
+        [Input("library-datatable", "active_cell"),
+         Input("library-datatable", "derived_viewport_data")]
     )
     def update_library_modal(active_cell, data):
         if active_cell is None:
             return [dash.no_update, dash.no_update, dash.no_update]
         if active_cell["column"] == 1:
-            return [True, authors[data[active_cell["row"]]["Author"]]["description"], data[active_cell["row"]]["Author"]]
+            return [True, authors[data[active_cell["row"]]["Author"]]["description"],
+                    data[active_cell["row"]]["Author"]]
         return [dash.no_update, dash.no_update, dash.no_update]
